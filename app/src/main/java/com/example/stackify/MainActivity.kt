89ab15899afc36903c925c20c8a68a790ff7ef2e
captain82 <nktkumar82@gmail.com>
2021-08-04 09:19:01 +0530

    Mic Drop.Ready to launch

--- app/src/main/java/com/example/stackify/MainActivity.kt
+++ app/src/main/java/com/example/stackify/MainActivity.kt
@@ -18,6 +18,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var creditAmountBottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
     private lateinit var bankBottomSheet: BottomSheetBehavior<ConstraintLayout>
     private lateinit var paymentBottomSheet: BottomSheetBehavior<ConstraintLayout>
+    private var creditAmountOffset = 1000
+    private var paymentOffset = creditAmountOffset + 200
+    private var bankOffset = paymentOffset + 200
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,12 +30,7 @@ class MainActivity : AppCompatActivity() {
         addViewToStack(R.layout.credit_amount_layout, "CREDIT-LAYOUT", 1)
         creditAmountBottomSheetBehavior = BottomSheetBehavior.from(proceedToEmiBottomSheet)
         proceedToEmiButton.setOnClickListener {
-            if (viewStack.isEmpty())
-                viewStack.add(ViewState("CREDIT-LAYOUT", 1))
-            creditAmountBottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
-            creditAmountBottomSheetBehavior.isDraggable = false
-            addViewToStack(R.layout.pay_layout, "PAYMENT-LAYOUT", 2)
-            paymentBottomSheet = BottomSheetBehavior.from(pleasePayBottomSheet)
+            setUpCreditAmountBottomSheet()
         }
 
         creditAmountBottomSheetBehavior.addBottomSheetCallback(object :
@@ -40,32 +38,7 @@ class MainActivity : AppCompatActivity() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {
                 if (newState == BottomSheetBehavior.STATE_EXPANDED) {
                     pleasePay.setOnClickListener {
-                        viewStack.add(ViewState("PAYMENT-LAYOUT", 2))
-                        paymentBottomSheet.expandedOffset = 1250
-                        paymentBottomSheet.isFitToContents = false
-                        paymentBottomSheet.isDraggable = false
-                        paymentBottomSheet.state = BottomSheetBehavior.STATE_EXPANDED
-                        addViewToStack(R.layout.select_bank_layout, "BANK_LAYOUT", 3)
-                        bankBottomSheet = BottomSheetBehavior.from(selectBankBottomSheetLayout)
-                        bankBottomSheet.isDraggable = false
-
-                        paymentBottomSheet.addBottomSheetCallback(object :
-                            BottomSheetBehavior.BottomSheetCallback() {
-                            override fun onStateChanged(bottomSheet: View, newState: Int) {
-                                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
-                                    selectBankButton.setOnClickListener {
-                                        viewStack.add(ViewState("BANK_LAYOUT", 3))
-                                        bankBottomSheet.expandedOffset = 1450
-                                        bankBottomSheet.isFitToContents = false
-                                        bankBottomSheet.state = BottomSheetBehavior.STATE_EXPANDED
-                                    }
-                                }
-                            }
-
-                            override fun onSlide(bottomSheet: View, slideOffset: Float) {
-
-                            }
-                        })
+                        setUpPaymentAmountBottomSheet()
                     }
                 }
             }
@@ -73,6 +46,51 @@ class MainActivity : AppCompatActivity() {
             override fun onSlide(bottomSheet: View, slideOffset: Float) {
             }
         })
+    }
+
+    private fun setUpPaymentAmountBottomSheet() {
+        viewStack.add(ViewState("PAYMENT-LAYOUT", 2))
+        paymentBottomSheet.expandedOffset = paymentOffset
+        paymentBottomSheet.isFitToContents = false
+        paymentBottomSheet.isDraggable = false
+        paymentBottomSheet.state = BottomSheetBehavior.STATE_EXPANDED
+        addViewToStack(R.layout.select_bank_layout, "BANK_LAYOUT", 3)
+        bankBottomSheet = BottomSheetBehavior.from(selectBankBottomSheetLayout)
+        bankBottomSheet.isDraggable = false
+
+        paymentBottomSheet.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                    selectBankButton.setOnClickListener {
+                        setUpBankBottomSheet()
+                    }
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+            }
+        })
+    }
+
+    private fun setUpBankBottomSheet() {
+        viewStack.add(ViewState("BANK_LAYOUT", 3))
+        bankBottomSheet.expandedOffset = bankOffset
+        bankBottomSheet.isFitToContents = false
+        bankBottomSheet.state = BottomSheetBehavior.STATE_EXPANDED
+    }
+
+    private fun setUpCreditAmountBottomSheet() {
+        if (viewStack.isEmpty())
+            viewStack.add(ViewState("CREDIT-LAYOUT", 1))
+        creditAmountBottomSheetBehavior.expandedOffset = creditAmountOffset
+        creditAmountBottomSheetBehavior.isFitToContents = false
+        creditAmountBottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        creditAmountBottomSheetBehavior.isDraggable = false
+        addViewToStack(R.layout.pay_layout, "PAYMENT-LAYOUT", 2)
+        paymentBottomSheet = BottomSheetBehavior.from(pleasePayBottomSheet)
+        paymentBottomSheet.isDraggable = false
     }
 
     private fun addViewToStack(layout: Int, viewTag: String, position: Int) {
